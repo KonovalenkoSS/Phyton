6047547b43773a79f6bd1f2b40470102aa3460da
KonovalenkoSS <bugimem13@gmail.com>
2020-05-19 15:57:28 +0500

    15-05

--- 08-05_2.py
+++
@@ -1,5 +0,0 @@
-first_list = [55, 3, 35, 45, 2, 13, 66]
-#first_list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
-second_list = [el for num, el in enumerate(first_list) if first_list[num - 1] < first_list[num]]
-print(f'Исходный список {first_list}')
-print(f'Новый список {second_list}')
--- 08-05_5.py
+++
@@ -1,8 +0,0 @@
-from functools import reduce
-
-
-def my_func(el_p, el):
-    return el_p * el
-
-print(f'Список {[el for el in range(100, 1000) if el % 2 == 0]}')
-print(f'Произведение всех значений {reduce(my_func, [el for el in range(100, 1000) if el % 2 == 0])}')
